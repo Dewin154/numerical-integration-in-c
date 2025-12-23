94e2631061d548974470fa6c3ac64e28529ed90a
Peter Okruhlica <peterokruhlica@gmail.com>
2025-12-23 08:00:25 +0100

    simple function pointer implemented

--- NumericalIntegration/NumericalIntegration/main.c
+++ NumericalIntegration/NumericalIntegration/main.c
@@ -6,26 +6,40 @@
 
 
 // 4x^4 + 7x^3 - 4x + 14
-float function1(x)
+float function1(float x)
 {
 	return (float) (4 * x * x * x * x) + (7 * x * x * x) - (4 * x) + 14;
 }
 
 
 // sin x - 7x + 8
-float function2(x)
+float function2(float x)
 {
 	return (float)(sin(x) - 7 * x + 8);
+}
+
+float function3(float (*f)(float x), float *rResult)
+{
+	*rResult = f(2.5);
+	return 0;
 }
 
 void main(void)
 {
 
-	float var = function1(5);
+	float var = function1(2.5);
 
-	printf("%f\n", var);
+	//printf("%f\n", var);
 
-	printf("%f", function2(7));
+	printf("%f\n", function1(2.5));
+	
+	float result = 0;
+	float* pResult = &result;
+	float (*pFunc)(float x) = &function1;
+
+	function3(pFunc, pResult);
+
+	printf("result is: %f\n", result);
 
 
 	while (!_kbhit())
